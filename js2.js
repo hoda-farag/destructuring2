13a33533d6a3858b12563bd5786c06cd17280a86
Hoda Farag <76969231+hoda-farag@users.noreply.github.com>
2022-07-24 15:18:27 +0200

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,18 @@
+let myInfo=
+{
+    age:22,
+    working :false,
+    country:"Egypt",
+    hobbies:["reading" , "swimming" , "programing"]
+};
+ 
+let a=myInfo.age
+let w=myInfo.working;
+let c=myInfo.country;
+let h1=myInfo.hobbies[0];
+ let h2=myInfo.hobbies[2];
+ console.log(`my age is ${a} and Iam ${w ?"":"not "} working`);
+
+ console.log(`I live in ${c}`);
+
+ console.log(`my hobbies is:${h1} and ${h2}`);
